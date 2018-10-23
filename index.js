7955d5df72e67367675b7bd114b6d672866ac6a8
Guy <guythomas721@gmail.com>
2018-10-22 22:14:52 -0700

    basics

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static('public'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(process.env.PORT)
